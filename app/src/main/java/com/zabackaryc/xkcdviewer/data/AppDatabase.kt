499720da59918f0070fb6fc39e531604c1cd2945
Zachary C <not.an.email@example.com>
2023-05-13 10:34:35 +0900

    Upgrade deps

--- app/src/main/java/com/zabackaryc/xkcdviewer/data/AppDatabase.kt
+++ app/src/main/java/com/zabackaryc/xkcdviewer/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.zabackaryc.xkcdviewer.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [CachedComic::class, ListedComic::class, HistoryEntry::class], version = 1)
+@Database(entities = [CachedComic::class, ListedComic::class, HistoryEntry::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getAppDao(): ComicDao
 }
